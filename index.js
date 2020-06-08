53f7f983ef0739437554cfb00af66a4f1637a06c
Arnur Kupanov <ncase01@gmail.com>
2020-06-08 15:02:17 +0600

    added callbacks

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 class Timer {
-    constructor(durationInput, startBtn, pauseBtn) {
+    constructor(durationInput, startBtn, pauseBtn, callbacks) {
         this.durationInput = durationInput
         this.startBtn = startBtn
         this.pauseBtn = pauseBtn
+
+        if (callbacks) {
+            this.onStart = callbacks.onStart
+            this.onTick = callbacks.onTick
+            this.onComplete = callbacks.onComplete
+        }
 
         this.startBtn.addEventListener('click', this.start)
         this.pauseBtn.addEventListener('click', this.pause)
     }
 
     start = () => {
+        this.onStart && this.onStart()
         this.tick()
         this.interval = setInterval(this.tick, 1000)
     }
 
     pause = () => {
+        this.onComplete && this.onComplete()
         clearInterval(this.interval)
     }
 
     tick = () => {
-        this.timerRemaining <= 0 ?
+        if (this.timerRemaining <= 0) {
             this.pause()
-            :
+        }
+        else {
             this.timerRemaining -= 1
+            this.onTick && this.onTick()
+        }
+
     }
 
     get timerRemaining() {
@@ -37,4 +49,14 @@ const durationInput = document.querySelector('#duration')
 const startBtn = document.querySelector('#start')
 const pauseBtn = document.querySelector('#pause')
 
-const timer = new Timer(durationInput, startBtn, pauseBtn)
+const timer = new Timer(durationInput, startBtn, pauseBtn, {
+    onStart() {
+        console.log('Start')
+    },
+    onTick() {
+        console.log('Tick')
+    },
+    onComplete() {
+        console.log('Complete')
+    }
+})
